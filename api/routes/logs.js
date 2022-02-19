dcf150527911e6833cf8576b2eef68a2103eeb79
Konrad (PC) <kolego199@gmail.com>
2022-02-19 19:45:11 +0100

    add num entries to GET /logs

--- api/routes/logs.js
+++ api/routes/logs.js
@@ -8,28 +8,32 @@ const ERROR_LOG_FILE = "error.log";
 
 router
   // GET all log files
-  .get("/", (req, res) => {
+  .get("/", (_req, res) => {
     fs.readFile(LOGS_DIRECTORY + ERROR_LOG_FILE, (fileErr, data) => {
       if (fileErr) {
         // The file could not be read
         return sendError(res, 500, fileErr);
       }
       // Replace Windows CRLF with regular linefeeds
-			const rawString = data.toString().replace(/\r\n/g, "\n");
-			// Split the string into an array of lines, ignoring empty lines
+      const rawString = data.toString().replace(/\r\n/g, "\n");
+      // Split the string into an array of lines, ignoring empty lines
       const lines = rawString.split("\n").filter((line) => line);
-			// Parse each line as a JSON object
+      // Parse each line as a JSON object
       const parsed = lines.map((line, index) => {
         try {
           return JSON.parse(line);
         } catch (parseErr) {
-					// The string could not be parsed; put in its place the error message
+          // The string could not be parsed; put in its place the error message
           const msg = parseErr.message;
           const errorDescription = `Error parsing line ${index + 1}: ${msg}.`;
           return { errorDescription };
         }
       });
-      sendOK(res, parsed);
+			// Send the formatted result JS object
+      sendOK(res, {
+        numEntries: parsed.length,
+        entries: parsed,
+      });
     });
   });
 
