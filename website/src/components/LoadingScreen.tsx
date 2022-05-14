20be5fd3a68aff10e5415da5a970a539119f5d52
Konrad (PC) <konrad@guzek.uk>
2022-05-14 23:53:35 +0200

    implement better-looking loading screen

--- website/src/components/LoadingScreen.tsx
+++ website/src/components/LoadingScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ThreeDots } from "react-loader-spinner";
+import "../styles/loadingScreen.css";
 
-export default function LoadingScreen({}) {
+export default function LoadingScreen() {
   return (
-    <div className="centred" style={{ marginTop: "35vh" }}>
-      <h2>Loading Guzek UK...</h2>
-			<ThreeDots />
+    <div className="loading-screen">
+      <h2 className="loading-text">Loading Guzek UK</h2>
+      <ThreeDots height={15} />
     </div>
   );
 }
