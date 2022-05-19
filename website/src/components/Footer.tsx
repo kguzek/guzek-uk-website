03bfe9aff1985ec72fa4d820538835a7a620d041
Konrad (PC) <konrad@guzek.uk>
2022-05-19 19:01:44 +0200

    change copyright text in footer

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -4,13 +4,11 @@ import "../styles/footer.css";
 import { Translation } from "../translations";
 
 function Footer({ data }: { data: Translation }) {
-  const copyrightSymbol = "\u00a9";
   return (
     <footer className="centred">
       <hr />
       <Logo size={20} />
       <small>
-        {copyrightSymbol}{" "}
         {data.footer.replace("{YEAR}", new Date().getFullYear().toString())}
       </small>
     </footer>
