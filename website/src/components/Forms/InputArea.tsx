1c42332889fc5253ac88aaacadb30c565aeb8116
Konrad (PC) <konrad@guzek.uk>
2022-07-17 23:23:14 +0200

    implement infrastructure for new api models
    also implement page content editor

--- website/src/components/Forms/InputArea.tsx
+++ website/src/components/Forms/InputArea.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import RichTextEditor, { EditorValue } from "react-rte";
 
-export default function InputArea() {
-  return <div>InputArea</div>;
+export const getEmptyMarkdown = RichTextEditor.createEmptyValue;
+
+export default function InputArea({
+  value,
+  setValue,
+}: {
+  value: EditorValue;
+  setValue: Function;
+}) {
+  function handleChange(newValue: any) {
+    setValue(newValue);
+  }
+
+  return <RichTextEditor value={value} onChange={handleChange} />;
 }
