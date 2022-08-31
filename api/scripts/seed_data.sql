68be12c33b606ff4825e6f54315a34739a67885e
Konrad (PC) <konrad@guzek.uk>
2022-08-31 20:06:43 +0200

    update misc data

--- api/scripts/seed_data.sql
+++ api/scripts/seed_data.sql
@@ -1,4 +1,23 @@
-INSERT INTO `guzek_uk`.`pages` (`title`, `url`, `admin_only`, `should_fetch`)
-VALUES ('Home', '/', FALSE, TRUE),
-  ('Konrad', '/konrad', FALSE, TRUE),
-  ('Pipe Designer', '/pipe-designer', FALSE, FALSE)
+INSERT INTO `guzek_uk`.`pages` (
+    `title`,
+    `url`,
+    `local_url`,
+    `admin_only`,
+    `should_fetch`
+  )
+VALUES ('Home', '/', TRUE, FALSE, TRUE),
+  ('Konrad', '/konrad', TRUE, FALSE, TRUE),
+  (
+    'Pipe Designer',
+    '/pipe-designer',
+    FALSE,
+    FALSE,
+    FALSE
+  ),
+  (
+    'Content Manager',
+    '/content-manager',
+    TRUE,
+    TRUE,
+    FALSE
+  )
