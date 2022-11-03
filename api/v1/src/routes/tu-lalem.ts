03f6f2a1027f9bd28b561c005eb72a73cd743ff1
Konrad Guzek <konrad@guzek.uk>
2022-11-03 10:20:54 +0100

    add coordinate validation boilerplate

--- api/v1/src/routes/tu-lalem.ts
+++ api/v1/src/routes/tu-lalem.ts
@@ -6,12 +6,36 @@ import {
   readAllDatabaseEntries,
   readDatabaseEntry,
   sendOK,
+	sendError,
 } from "../util";
 export const router = express.Router();
 
+/** Ensures that the coordinates are not to close to any previous entries.
+ *  Returns an error message or `null` if the coordinates are valid.
+ */
+async function validateCoordinates(coords?: number[]): string | null {
+	if (!coords) return "Coordinates not provided.";
+	if (coords.length < 2) return "Coordinates must have a latitude and longitude.";
+	// TODO: implement actual validation
+	return null;
+}
+
 router
-  .post("/", (req, res) => {
-    createDatabaseEntry(TuLalem, req, res);
+  .post("/", async (req, res) => {
+		const coords = req.body.coordinates;
+		const errorMessage = await validateCoordinates(coords);
+		if (errorMessage) {
+			return sendError(res, 400, { message: errorMessage });
+		}
+
+		const modelParams = {
+			timestamp: req.body.timestamp,
+			coordinates: {
+				type: "Point",
+				coordinates: req.body.coordinates,
+			},
+		};
+    await createDatabaseEntry(TuLalem, req, res, modelParams);
   })
 
   .get("/", async (req, res) => {
