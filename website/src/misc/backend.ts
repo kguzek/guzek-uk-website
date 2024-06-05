f059d811962e2a0ee5d8c98866c5740b08f0372e
Konrad Guzek <konrad@guzek.uk>
2024-06-05 23:44:00 +0200

    Cleaned up token refreshing code

--- website/src/misc/backend.ts
+++ website/src/misc/backend.ts
@@ -3,6 +3,7 @@ import { TryFetch } from "./util";
 
 const USE_LOCAL_API_URL = false;
 const CACHE_NAME = "guzek-uk-cache";
+const TOKEN_PENDING = "TOKEN_PENDING";
 
 interface RequestOptions {
   method: string;
@@ -39,49 +40,90 @@ export function getSearchParams(
   return `?${searchParams}`;
 }
 
-const isTokenRefreshing = () => localStorage.getItem("refreshingToken") != null;
+const isTokenPending = () => localStorage.getItem(TOKEN_PENDING) != null;
+
+let tokenPendingInThisTab = false;
+let accessTokenPromise: Promise<string | null> | undefined = undefined;
+
+function getAccessTokenInfo(): null | { accessToken?: string; expiresAt: string } {
+  const accessTokenInfo = localStorage.getItem("accessTokenInfo");
+  return accessTokenInfo ? JSON.parse(accessTokenInfo) : null;
+}
+
+const listenForTokenUpdate = (): Promise<string | null> => new Promise((resolve) => {
+  function listener(evt: StorageEvent) {
+    if (evt.key !== TOKEN_PENDING) return;
+    if (isTokenPending()) return;
+    window.removeEventListener("storage", listener);
+    const accessTokenInfo = getAccessTokenInfo();
+    const accessToken = accessTokenInfo?.accessToken || null;
+    resolve(accessToken);
+  }
+  window.addEventListener("storage", listener);
+});
+
+const toPromise = async <T>(resolvesTo: T): Promise<T> => resolvesTo;
 
 /** Determines the API access token and generates a new one if it is out of date. */
-async function getAccessToken(auth: Auth): Promise<null | string> {
-  const accessTokenInfo = localStorage.getItem("accessTokenInfo");
-  if (!accessTokenInfo) {
-    return null;
-  }
-  const tokenInfo = JSON.parse(accessTokenInfo);
+function getAccessToken(auth: Auth): Promise<null | string> {
+  const tokenInfo = getAccessTokenInfo();
+  if (!tokenInfo) return toPromise(null);
   // Check if it's expired
-  const tokenNotExpired = new Date(tokenInfo.expiresAt) > new Date();
-  if (tokenNotExpired && tokenInfo.accessToken) {
-    return tokenInfo.accessToken as string;
+  const tokenExpired = new Date() > new Date(tokenInfo.expiresAt);
+  const tokenValid = tokenInfo.accessToken != null;
+  if (tokenValid && !tokenExpired) {
+    return toPromise(tokenInfo.accessToken as string);
   }
-  if (isTokenRefreshing()) {
-    while (isTokenRefreshing()) {
-      await new Promise((resolve) => setTimeout(resolve, 0));
+  // Token expired
+  if (isTokenPending()) { 
+    // Token being refreshed currently
+    if (tokenPendingInThisTab) {
+      if (!accessTokenPromise) throw new Error("Token pending but no promise set");
+    } else {
+      // Wait until it completes in other tab
+      accessTokenPromise = listenForTokenUpdate();
     }
-    console.info("Token refreshed!");
-    return await getAccessToken(auth);
+  } else {
+    // Token needs to be refreshed here
+    accessTokenPromise = refreshAccessToken(auth);
   }
+  return accessTokenPromise;
+}
+
+/* Sends a request to the API to refresh the access token using the refresh token. */
+async function refreshAccessToken(auth: Auth): Promise<null | string> {
   // Generate a new access token
-  localStorage.setItem("refreshingToken", "true");
-  const token = localStorage.getItem("refreshToken") ?? "";
+  localStorage.setItem(TOKEN_PENDING, TOKEN_PENDING);
+  tokenPendingInThisTab = true;
+  const refreshToken = localStorage.getItem("refreshToken") ?? "";
+  if (!refreshToken) return null;
   const req = await createRequest(
     auth,
     "auth/access",
     "POST",
-    { body: { token } },
+    { body: { token: refreshToken } },
     false
   );
   console.info("Refreshing expired access token...");
-  const res = await fetch(req);
+  let res;
+  try {
+    res = await fetch(req);
+  } catch {
+    console.error("Could not fulfil request to refresh access token (API is probably offline).");
+    localStorage.removeItem(TOKEN_PENDING);
+    return null;
+  }
   if (!res.ok) {
-    console.error("Failed to refresh the access token. Logging out.");
+    console.error("Failed to refresh the access token (token probably invalid). Logging out.");
     clearStoredLoginInfo();
-    localStorage.removeItem("refreshingToken");
+    localStorage.removeItem(TOKEN_PENDING);
     auth.logout();
     return null;
   }
   const body = await res.json();
   updateAccessToken(body.accessToken);
-  localStorage.removeItem("refreshingToken");
+  localStorage.removeItem(TOKEN_PENDING);
+  tokenPendingInThisTab = false;
   return body.accessToken as string;
 }
 
