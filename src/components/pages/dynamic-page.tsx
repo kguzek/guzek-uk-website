c095b1f4488760f05dab4690a5eeeb1782e2abb6
Konrad Guzek <konrad@guzek.uk>
2025-01-06 00:22:58 +0100

    update error handling for pages

--- src/components/pages/dynamic-page.tsx
+++ src/components/pages/dynamic-page.tsx
@@ -12,25 +12,16 @@ export async function getPageBySlug(slug: string) {
   return result.data.find((item) => item.shouldFetch && item.url === slug);
 }
 
-async function getContentById(id: number) {
-  const result = await serverToApi<PageContent>(`pages/${id}`);
-  if (!result.ok) {
-    console.error("Failed to fetch page content", result);
-    return null;
-  }
-  return result.data;
-}
-
 export async function DynamicPageLoader({ page }: { page: string }) {
   const currentPage = await getPageBySlug(page);
   if (!currentPage) return <ErrorComponent errorCode={ErrorCode.NotFound} />;
-  const content = await getContentById(currentPage.id);
-  if (!content) return <ErrorComponent errorCode={ErrorCode.ServerError} />;
+  const result = await serverToApi<PageContent>(`pages/${currentPage.id}`);
+  if (!result.ok) return <ErrorComponent errorResult={result} />;
   return (
     <div className="text flex justify-center">
       <div
         className="page-content prose mt-6"
-        dangerouslySetInnerHTML={{ __html: content.content }}
+        dangerouslySetInnerHTML={{ __html: result.data.content }}
       ></div>
     </div>
   );
