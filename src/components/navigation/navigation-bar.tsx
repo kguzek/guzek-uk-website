bf66faf67485bee00e789b455a308bbd684ac17f
Konrad Guzek <konrad@guzek.uk>
2025-01-25 00:08:00 +0100

    fix nav menu z-index

--- src/components/navigation/navigation-bar.tsx
+++ src/components/navigation/navigation-bar.tsx
@@ -22,7 +22,7 @@ export async function NavigationBar() {
           {/* Hamburger */}
           <label
             aria-controls="menu"
-            className="peer z-20 flex cursor-pointer flex-col justify-center p-4 lg:hidden"
+            className="peer z-30 flex cursor-pointer flex-col justify-center p-4 lg:hidden"
           >
             <input
               type="checkbox"
@@ -38,10 +38,10 @@ export async function NavigationBar() {
           <label
             htmlFor="hamburger"
             aria-controls="menu"
-            className="pointer-events-none fixed left-0 top-0 h-[100vh] w-[100vw] opacity-0 backdrop-blur-[6px] transition-opacity duration-300 peer-has-[:checked]:pointer-events-auto peer-has-[:checked]:opacity-50 lg:hidden"
+            className="pointer-events-none fixed left-0 top-0 z-10 h-[100vh] w-[100vw] opacity-0 backdrop-blur-[6px] transition-opacity duration-300 peer-has-[:checked]:pointer-events-auto peer-has-[:checked]:opacity-50 lg:hidden"
           ></label>
           {/* Menu */}
-          <ul className="invisible absolute right-0 top-0 z-10 w-full origin-top-right scale-[25%] transform select-none items-center gap-3 rounded-b-lg border-0 border-b-2 border-background-soft bg-background pb-4 opacity-0 shadow-lg shadow-background-strong transition-[opacity,transform,visibility] duration-300 peer-has-[:checked]:visible peer-has-[:checked]:scale-100 peer-has-[:checked]:opacity-100 sm:right-10 sm:top-3 sm:w-[50%] sm:rounded-lg sm:border-2 sm:border-solid lg:visible lg:static lg:flex lg:w-full lg:transform-none lg:border-none lg:bg-transparent lg:opacity-100 lg:shadow-none">
+          <ul className="invisible absolute right-0 top-0 z-20 w-full origin-top-right scale-[25%] transform select-none items-center gap-3 rounded-b-lg border-0 border-b-2 border-background-soft bg-background pb-4 opacity-0 shadow-lg shadow-background-strong transition-[opacity,transform,visibility] duration-300 peer-has-[:checked]:visible peer-has-[:checked]:scale-100 peer-has-[:checked]:opacity-100 sm:right-10 sm:top-3 sm:w-[50%] sm:rounded-lg sm:border-2 sm:border-solid lg:visible lg:static lg:flex lg:w-full lg:transform-none lg:border-none lg:bg-transparent lg:opacity-100 lg:shadow-none">
             <div className="mt-5 flex justify-center sm:hidden">
               <UserWidget user={user} userLanguage={userLanguage} />
             </div>
