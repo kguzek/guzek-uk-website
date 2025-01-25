0820fdd070607e75308c6c634a4429e7b6744fde
Konrad (PC) <konrad@guzek.uk>
2025-01-26 00:42:00 +0100

    add url log

--- src/middleware/redirect-middleware.ts
+++ src/middleware/redirect-middleware.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import type { MiddlewareFactory } from "@/lib/types";
 
 export const redirectMiddleware: MiddlewareFactory = (next) =>
-  async function (request) {
-    if (request.nextUrl.host === "guzek.uk") {
+  function (request) {
+    console.log(request.nextUrl.href, request.nextUrl.hostname);
+    if (request.nextUrl.hostname === "guzek.uk") {
       return NextResponse.redirect(
         new URL("https://www.guzek.uk" + request.nextUrl.pathname),
       );
