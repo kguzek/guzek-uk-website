8470382a24bcbc6cbce1e14a4ad480c77f60c308
Konrad (PC) <konrad@guzek.uk>
2025-01-26 01:02:04 +0100

    fix www redirect

--- src/middleware/redirect-middleware.ts
+++ src/middleware/redirect-middleware.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 import type { MiddlewareFactory } from "@/lib/types";
 
 export const redirectMiddleware: MiddlewareFactory = (next) =>
-  function (request) {
-    console.log(request.nextUrl.href, request.nextUrl.hostname);
-    if (request.nextUrl.hostname === "guzek.uk") {
+  async function (request) {
+    const requestHeaders = await headers();
+    const host = requestHeaders.get("host");
+    if (host === "guzek.uk") {
       return NextResponse.redirect(
-        new URL("https://www.guzek.uk" + request.nextUrl.pathname),
+        new URL(
+          "https://www.guzek.uk" +
+            request.nextUrl.pathname +
+            request.nextUrl.search,
+        ),
       );
     }
     return next(request);
