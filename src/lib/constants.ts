3e3cace13f8af3ae2284ea7cc88991f7c94d230a
Konrad Guzek <konrad@guzek.uk>
2025-02-23 04:15:19 +0100

    allow website url override

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const PAGINATED_REGEX_INVALID =
 
 export const PAGINATED_REGEX = /^(\/liveseries\/(?:search\/[^\/]+|most-popular))\/\d+?$/;
 
-export const PRODUCTION_URL = "https://www.guzek.uk";
+export const PRODUCTION_URL = process.env.WEBSITE_URL || "https://www.guzek.uk";
