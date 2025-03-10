8359cbb4b8ed0ddcf77489ae986da7dad02629b9
Konrad (PC) <konrad@guzek.uk>
2025-03-10 22:48:46 +0100

    remove beta footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -21,7 +21,7 @@ const TV_MAZE_URL = "https://www.tvmaze.com/";
 const REPO_NAME = "guzek-uk-website";
 
 const BETA_INFO = {
-  enabled: true,
+  enabled: false,
   url: `${GITHUB_URL}/${REPO_NAME}/pull/19`,
   label: "next-intl",
 };
