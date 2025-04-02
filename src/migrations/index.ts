0b20c6e83ec3c3e78101d150199b4cb261c3fe79
Konrad Guzek <konrad@guzek.uk>
2025-04-02 16:06:21 +0200

    update reset migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250402_131102 from './20250402_131102';
+import * as migration_20250402_140541 from './20250402_140541';
 
 export const migrations = [
   {
-    up: migration_20250402_131102.up,
-    down: migration_20250402_131102.down,
-    name: '20250402_131102'
+    up: migration_20250402_140541.up,
+    down: migration_20250402_140541.down,
+    name: '20250402_140541'
   },
 ];
