adf6dc1a81439b67e1a60deab586e38930842177
Konrad Guzek <konrad@guzek.uk>
2026-03-22 23:37:27 +0100

    fix(projects): schedule width update

--- src/lib/hooks/embla-scroll.ts
+++ src/lib/hooks/embla-scroll.ts
@@ -7,29 +7,35 @@ type Api = Exclude<EmblaApi, undefined>;
 export function useEmblaScroll(api: EmblaApi) {
   const [state, setState] = useState({ progress: 0, total: 1 });
 
+  const updateProgress = (api: Api) => {
+    setState((prev) => ({ ...prev, progress: api.scrollProgress() }));
+  };
+
+  const updateTotal = (api: Api) => {
+    setState((prev) => ({ ...prev, total: api.slideNodes().length || 1 }));
+  };
+
+  const update = (api: Api) => {
+    updateTotal(api);
+    updateProgress(api);
+  };
+
   useEffect(() => {
     if (api == null) {
       return;
-    }
-    function updateProgress(api: Api) {
-      setState((prev) => ({ ...prev, progress: api.scrollProgress() }));
-    }
-    function updateTotal(api: Api) {
-      setState((prev) => ({ ...prev, total: api.slideNodes().length || 1 }));
-    }
-    function update(api: Api) {
-      updateTotal(api);
-      updateProgress(api);
     }
 
     api.on("scroll", updateProgress);
     api.on("init", update);
 
-    update(api);
+    const handle = requestAnimationFrame(() => {
+      update(api);
+    });
 
     return () => {
       api.off("scroll", updateProgress);
       api.off("init", update);
+      cancelAnimationFrame(handle);
     };
   }, [api]);
 
